8fe8d809e3f683837dcb55bce9bf2f84d58e14b7
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-04 20:27:58 -0300

    Testando o Relacionamento entre Conta e Cliente

--- src/main/java/br/com/alura/jpa/modelo/Cliente.java
+++ src/main/java/br/com/alura/jpa/modelo/Cliente.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -22,6 +23,7 @@ public class Cliente {
 	private String endereco;
 	
 	@OneToOne
+	@JoinColumn(unique = true)
 	private Conta conta;
 
 	public Long getId() {
