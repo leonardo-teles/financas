f0ec34e1d8c6455c840282060e2a380005fbc5d4
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2020-05-04 21:38:01 -0300

    Query com ORDER BY DESC

--- src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
+++ src/main/java/br/com/alura/jpa/testes/TesteJPQL.java
@@ -5,7 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import br.com.alura.jpa.modelo.Conta;
 import br.com.alura.jpa.modelo.Movimentacao;
@@ -17,17 +17,15 @@ public class TesteJPQL {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
 		EntityManager em = emf.createEntityManager();
 		
+		String jpql = "select m from Movimentacao m where m.conta = :pConta order by m.valor desc";
+
 		Conta conta = new Conta();
 		conta.setId(2L);
 		
-		String jpql = "select m from Movimentacao m where m.conta = :pConta";
-		
-		Query query =  em.createQuery(jpql);
+		TypedQuery<Movimentacao> query = em.createQuery(jpql, Movimentacao.class);
 		query.setParameter("pConta", conta);
 		
-		@SuppressWarnings("unchecked")
 		List<Movimentacao> resultList = query.getResultList();
-		
 		
 		for (Movimentacao movimentacao : resultList) {
 			System.out.println("Descrição: " + movimentacao.getDescricao());
